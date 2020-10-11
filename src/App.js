a78058ecd4992fb2c88a887be7697d38990fb2f7
Jovany Vélez <jovany.velez@udea.edu.co>
2020-10-11 01:46:01 -0500

    create product component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
+import data from './data.json'
+import Productos from './components/ProductosComponent'
 
 
+class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      productos: data.productos,
+      talla:"",
+      ordenar:"",
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Carro de compras</a>
-      </header>
-      <main>
-        Lista de Productos
-      </main>
-      <footer>allright is reserved.</footer>
-    </div>
-  );
+    }
+
+  }
+  
+  render (){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Carro de compras</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Productos productos = {this.state.productos}/>
+            </div>
+            <div className="sidebar">
+              Items carrito
+            </div>
+          </div>
+        </main>
+        <footer>allright is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
