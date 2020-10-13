bd5a1409447327fb9cf1632245388362647a449d
Jovany Vélez <jovany.velez@udea.edu.co>
2020-10-13 00:06:23 -0500

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from './data.json'
 import Productos from './components/ProductosComponent'
+import Filter from './components/Filter';
 
 
 class App extends React.Component {
@@ -11,10 +12,75 @@ class App extends React.Component {
       productos: data.productos,
       talla:"",
       ordenar:"",
-
     }
-
   }
+
+
+  ordenarProductos = (e)=>{
+    console.log(e.target.value)
+    const ordenar = e.target.value
+    switch (ordenar) {
+      case 'lowest':
+        this.setState({
+          ordenar:ordenar,
+          productos: this.state.productos.sort((a,b)=>a.precio-b.precio)
+        })
+        break
+      case 'highest':
+        this.setState({
+          ordenar:ordenar,
+          productos: this.state.productos.sort((a,b)=>b.precio-a.precio)
+        })
+        break;
+      default:
+          this.setState({
+            ordenar:ordenar,
+            productos: this.state.productos.sort((a,b)=>
+              a._id<b._id?-1:1
+            )
+          })
+    }
+    
+  }
+    
+
+
+    
+ /*    const sort = e.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      productos: this.state.productos
+        .slice()
+        .sort((a,b)=>
+          sort==="lowest"
+          ?a.precio > b.precio
+            ? 1
+            :-1
+          :sort === "highest"
+          ? a.precio>b.precio
+            ? 1
+            :-1 
+          : a._id > b._id
+            ? 1
+            :-1
+        ),
+    })); */
+   //implement
+
+
+  filtrarProductos = (e)=>{
+    if(e.target.value===""){
+      this.setState({talla: e.target.value, 
+        productos: data.productos})
+    }else
+    {
+      this.setState({talla: e.target.value, 
+        productos: data.productos.filter(
+          producto => producto.tallasDisponibles.indexOf(e.target.value)>=0)})
+      console.log(e.target.value)
+      }
+  } //implement
+
   
   render (){
     return (
@@ -25,6 +91,12 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter count={this.state.productos.length}
+                      talla={this.state.talla}
+                      ordenar={this.state.ordenar}
+                      filtrarProductos= {this.filtrarProductos}
+                      ordenarProductos= {this.ordenarProductos}
+              />
               <Productos productos = {this.state.productos}/>
             </div>
             <div className="sidebar">
